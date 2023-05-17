fc581bfa7410adf33a16b56309fb2006669d2231
디자이너 <crazyupdesigner@dijaineoui-iMac.local>
2023-05-17 13:39:48 +0900

    hero

---
+++ src/pages/hero-heading-left.tsx
@@ -0,0 +1,35 @@
+import { FunctionComponent } from "react";
+import styles from "./hero-heading-left.module.css";
+const HeroHeadingLeft: FunctionComponent = () => {
+  return (
+    <div className={styles.heroHeadingLeft}>
+      <div className={styles.container}>
+        <div className={styles.column}>
+          <div className={styles.content}>
+            <b className={styles.h1TitleCopyGoes}>
+              Title Copy Goes Here Be Awesome
+            </b>
+            <div className={styles.pLoremIpsumDolor}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse varius enim in eros elementum tristique. Duis cursus,
+              mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam
+              libero vitae erat.
+            </div>
+          </div>
+          <div className={styles.actions}>
+            <div className={styles.button}>
+              <div className={styles.getStarted}>GET STARTED</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.column1}>
+          <div className={styles.imageWrapper}>
+            <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroHeadingLeft;
